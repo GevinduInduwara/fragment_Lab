9b9b310de21f2b90c05749700bd01a0bf204b2c2
Gevindu <gevinduinduwara28@gmail.com>
2023-03-16 17:13:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgHome: ImageView = findViewById(R.id.imgHome)
+        val imgUser:ImageView = findViewById(R.id.imgUser)
+        val fragmentHome = HomeFragment()
+        val fragmentUser = UserFragement()
+        imgUser.setOnClickListener {
+            imgHome.setImageResource(R.drawable.unselected_home)
+            imgUser.setImageResource(R.drawable.selected_user)
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView4, fragmentUser)
+                commit()
+            }
+        }
+        imgHome.setOnClickListener {
+            imgHome.setImageResource(R.drawable.selected_home)
+            imgUser.setImageResource(R.drawable.unselected_user)
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView4, fragmentHome)
+                commit()
+            }
+        }
+    }
+}
